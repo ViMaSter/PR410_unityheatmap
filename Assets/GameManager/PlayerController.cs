cc32cacf19d2417f6954434861d83eeb854e47be
Vincent Mahnke <vincent@mahn.ke>
2018-11-15 18:22:35 +0100

    Added visual feedback to laser

--- Assets/GameManager/PlayerController.cs
+++ Assets/GameManager/PlayerController.cs
@@ -10,10 +10,18 @@ public class PlayerController : MonoBehaviour {
 	public float falloutRatio = 0.15f;
 	[Range(0f, 1f)]
 	public float rotationLerp = 0.3f;
+	[Range(0f, 1f)]
+	public float shootLerp = 0.75f;
+	[Range(0f, 1f)]
+	public float shootPause = 0.75f;
+
+	IEnumerator currentFade = null;
 
 	private Vector2 currentMovement = Vector3.zero;
 
 	private new Rigidbody2D rigidbody2D;
+
+	public Transform laser;
 
 	void Awake()
 	{
@@ -36,12 +44,50 @@ public class PlayerController : MonoBehaviour {
 		{
 			Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 			mousePosition.z = transform.position.z;
-			RaycastHit2D hit = Physics2D.Raycast(transform.position, mousePosition - transform.position, Mathf.Infinity, ~(1 << 9));
-			if (hit.transform && hit.transform.gameObject.layer == 8 && hit.collider != null)
-			{
-				hit.transform.GetComponent<Hittable>().Hit((hit.transform.position - transform.position).normalized);
-			}
+			Shoot(mousePosition);
 		}
+	}
+
+	void Shoot(Vector3 target)
+	{
+		laser.rotation = Quaternion.LookRotation(transform.forward, transform.position - target) * Quaternion.Euler(0, 0, 90);
+
+		RaycastHit2D hit = Physics2D.Raycast(transform.position, target - transform.position, Mathf.Infinity, ~(1 << 9));
+		laser.localScale = new Vector3(Vector2.Distance(transform.position, hit.transform.position), 1, 1);
+		laser.position = Vector2.Lerp(transform.position, hit.transform.position, 0.5f);
+		if (hit.transform && hit.transform.gameObject.layer == 8 && hit.collider != null)
+		{
+			hit.transform.GetComponent<Hittable>().Hit((hit.transform.position - transform.position).normalized);
+		}
+
+		if (currentFade != null)
+		{
+			StopCoroutine(currentFade);
+		}
+		currentFade = Fade();
+		StartCoroutine(currentFade);
+	}
+
+	IEnumerator Fade()
+	{
+		SpriteRenderer renderer = laser.GetComponent<SpriteRenderer>();
+
+		Color newColor = renderer.color;
+		newColor.a = 1.0f;
+
+		renderer.color = newColor;
+	
+		yield return new WaitForSeconds(shootPause);
+
+		while (renderer.color.a > 0)
+		{
+			newColor.a = Mathf.Lerp(renderer.color.a, 0, shootLerp);
+	
+			renderer.color = newColor;
+			yield return new WaitForSeconds(0.025f);
+		}
+
+		currentFade = null;
 	}
 
 	void OnDrawGizmos()
